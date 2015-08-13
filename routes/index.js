98be7197526168398ce74155cbcf95bd9a818162
manayop <manayop@gmail.com>
2015-08-13 22:11:19 +0200

    Manejo de login y sesion

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller.js');
 var commentController = require('../controllers/comment_controller.js');
+var sessionController = require('../controllers/session_controller.js');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -15,6 +16,11 @@ router.get('/author', function(req, res) {
 
 //Autoload de comandos con quizId
 router.param('quizId',quizController.load);
+
+//rutas de sesion
+router.get('/login', sessionController.new); //formulario
+router.post('/login', sessionController.create); //crear sesion
+router.get('/logout', sessionController.destroy); //destruir sesion
 
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
