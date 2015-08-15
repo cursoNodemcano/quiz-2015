d8eaab519496b1fb1df9216ebc736b25791b62ba
manayop <manayop@gmail.com>
2015-08-15 11:46:11 +0200

    Autorizacion en lista de preguntas a usuarios autorizados

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,5 +1,14 @@
 
 
+//MW de autorizacion de accesos
+exports.loginRequired = function(req, res, next) {
+	if (req.session.user) {
+		next();
+	}else{
+		res.redirect('/login');
+	}
+};
+
 // GET /login
 exports.new = function(req, res) {
 	var errors = req.session.errors || {};
